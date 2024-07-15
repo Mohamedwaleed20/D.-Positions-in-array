b3eb62e3053550bfab360f04f53a788decfa5fc0
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-15 18:10:24 +0300

    Add project files.

---
+++ D. Positions in array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = Convert.ToInt32(arr[i]);
+            if (num <= 10)
+            {
+                Console.WriteLine("A[" + i + "] = " + num);
+            }
+        }
+    }
+}
